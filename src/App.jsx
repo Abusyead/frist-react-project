26b21eaef630753424b0e22378776a159df0ca28
Abusyead <abusyead1000@gmail.com>
2025-05-16 22:29:35 +0600

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <main>
+          <div className="w-full max-w-[1440px]">
+              <div className="space-y-[120px]">
+
+              </div>
+          </div>
+      </main>
+    </>
+  )
+}
+
+export default App
