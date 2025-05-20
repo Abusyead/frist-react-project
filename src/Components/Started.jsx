b7f4847506a4b31efc5bba73d6862cf43f63d4db
Abusyead <abusyead1000@gmail.com>
2025-05-20 21:07:38 +0600

    add  git structure in started section

---
+++ src/Components/Started.jsx
@@ -0,0 +1,18 @@
+import PinInvokeIcon from '@mui/icons-material/PinInvoke'
+function Started() {
+  return (
+    <section className="bg-black">
+        <div>
+            <h4>GET STARTED</h4>
+            <h3>Embrace the new era of outbound.</h3>
+            <p>
+                Wizia lets you train, activate, and       optimize aiDRs. 
+                Take your outbound to new levels of performance and efficiency.
+            </p>
+  
+        </div>
+    </section>
+  )
+}
+
+export default Started
